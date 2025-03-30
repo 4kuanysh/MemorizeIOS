cce66539f35c2abd7e8fecf08b410aa8eb24879b
Kuanysh Sultan <4kuanish@gmail.com>
2025-03-30 23:58:16 +0500

    GeometryReader and ViewBuilder

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Куаныш Султан on 30.03.2025.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
