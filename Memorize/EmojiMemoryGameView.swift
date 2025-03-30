282f49c21c8506fd966d50eeab021ea3b921f173
Kuanysh Sultan <4kuanish@gmail.com>
2025-03-30 23:58:39 +0500

    GeometryReader and ViewBuilder

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -10,33 +10,32 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
+    private let aspectRatio: CGFloat = 2/3
+
     var body: some View {
         VStack {
-            ScrollView {
-                cards.animation(.default, value: viewModel.cards)
-            }.padding()
+            cards
+                .animation(.default, value: viewModel.cards)
             Button("Shuffle") {
                 viewModel.shuffle()
             }
         }
+        .padding()
     }
     
-    var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
-                CardView(card: card)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
-            }
-        }
-        .foregroundColor(.green)
+    private var cards: some View {
+        AspectVGrid(items: viewModel.cards, aspectRatio: aspectRatio) { card in
+            CardView(card: card)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
+                }
+        }.foregroundColor(.green)
+    
     }
 }
 
-struct CardView: View {
+private struct CardView: View {
     let card: MemoryGame<String>.Card
     
     var body: some View {
